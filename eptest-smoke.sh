2f3ca871f6e3a45266453127760d9c7829f19c4d
fl-em <eugene@flexify.io>
2024-11-01 16:15:53 +0800

    Update eptest-smoke.sh
    Add email alert function sendmail()

--- eptest-smoke.sh
+++ eptest-smoke.sh
@@ -20,11 +20,49 @@ adddate() {
     while IFS= read -r line; do
         printf '%s %s\n' "$(date)" "$line";
     done }
+sendmail() {
+    local SENDGRID_API_KEY="SENDGRID_API_KEY"
+    local EMAIL_TO="$1"
+    local FROM_EMAIL="$2"
+    local FROM_NAME="$3"
+    local SUBJECT="$4"
+    local BODY="$5"
+
+    # Create the JSON payload
+    local maildata=$(cat <<EOF
+{
+  "personalizations": [{
+    "to": [{
+      "email": "$EMAIL_TO"
+    }]
+  }],
+  "from": {
+    "email": "$FROM_EMAIL",
+    "name": "$FROM_NAME"
+  },
+  "subject": "$SUBJECT",
+  "content": [{
+    "type": "text/html",
+    "value": "$BODY"
+  }]
+}
+EOF
+)
+
+    # Send the email using curl
+    curl --request POST \
+        --url https://api.sendgrid.com/v3/mail/send \
+        --header "Authorization: Bearer $SENDGRID_API_KEY" \
+        --header "Content-Type: application/json" \
+        --data "$maildata"
+}
 errch() {
     status=$?
     echo $status
     if [ $status != 0 ]; then
         echo $ts "Error encountered. Check the log for details."
+        sendmail "eugene@flexify.io" "sender@example.com" "Sender Name" "Subject of the Email" "<p>This is the email body.</p>"
+
     fi
 }
 eptest() {
